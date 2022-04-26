852eee9cfb9999dc4fecad884efd29781d04801a
RobertWooley <wooley278@morrisville.edu>
2022-04-26 14:34:01 -0400

    Filp Player Sprite

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -2,9 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class NewBehaviourScript : MonoBehaviour
 {
-
+    [SerializeField] float runspeed = 10f;
+    Vector2 moveInput;
     Rigidbody2D myRidgidbody;
     // Start is called before the first frame update
     void Start()
@@ -13,8 +15,34 @@ public class NewBehaviourScript : MonoBehaviour
     }
 
     // Update is called once per frame
+
+
     void Update()
     {
-        
+
+        Run();
     }
+
+    
+
+
+
+    
+
+    
+
+    
+
+
+
+
+    
+    
+    
+    
+  
+
+
+
+
 }
