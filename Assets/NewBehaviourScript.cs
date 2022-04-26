bd5a46efd7f3361fbf287dd4489098831e76a870
RobertWooley <wooley278@morrisville.edu>
2022-04-26 14:39:19 -0400

    An,mation State in code

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -8,41 +8,40 @@ public class NewBehaviourScript : MonoBehaviour
     [SerializeField] float runspeed = 10f;
     Vector2 moveInput;
     Rigidbody2D myRidgidbody;
-    // Start is called before the first frame update
+     //Start is called before the first frame update
     void Start()
     {
         myRidgidbody = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
+    //Update is called once per frame
 
 
-    void Update()
+    //void Update()
     {
 
-        Run();
+      //  Run();
+    }
+      Run();
     }
 
-    
+     //void OnMove(InputValue value)
+    {
+    //moveInput = value.Get<Vector2>();
+    //Debug.Log(moveInput);
+    }
+
+void Run()
+{
+    //Vector2 playerVelocity = new Vector2(moveInput.x * runSpeed, myRigidbody.velocity.y);
+    //myRigidbody.velocity = playerVelocity;
+}
 
 
 
-    
-
-    
-
-    
-
-
-
-
-    
-    
-    
-    
   
 
 
 
 
-}
+
